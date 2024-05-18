81ab886e6ed7e3cfd26c8de769d32669f2a5ad2b
CarrotUserBr <pablocenoura2017@gmail.com>
2024-05-18 19:45:40 -0300

    feat: system of time, advanced the print numbers of year, mounth and week

--- assets/scripts/time.js
+++ assets/scripts/time.js
@@ -8,9 +8,32 @@ modalAssistFunctions.printNumberOfAssistsInAlert()
 function changeTime() {
     modalAssistFunctions.generateNewAssistInstallItem()
     modalAssistFunctions.printNumberOfAssistsInAlert()
+    printTimeInHeader()
 }
 
+function printTimeInHeader() {
+    const year = document.getElementById('year')
+    const mounth = document.getElementById('mounth')
+    const week = document.getElementById('week')
+    let yearNumber = parseInt(year.textContent.split(' ')[1])
+    let mounthNumber = parseInt(mounth.textContent.split(' ')[1])
+    let weekNumber = parseInt(week.textContent.split(' ')[1])
+    weekNumber++
+    if (weekNumber > 4) {
+        weekNumber = 1
+        mounthNumber++
+    }
+    if (mounthNumber > 12) {
+        mounthNumber = 1
+        yearNumber++
+    }
+    year.textContent = `Ano ${yearNumber}`
+    mounth.textContent = `Mês ${mounthNumber}`
+    week.textContent = `Semana ${weekNumber}`
+
+}
 
 export default {
     changeTime,
+    printTimeInHeader,
 }
